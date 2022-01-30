b5f81ab69f484ccfeea75c5ddf1b44dc804c6209
Andranik007 <andranik.terteryan.98@list.ru>
2022-01-30 15:56:24 +0400

    task 2

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-let
-    arr = [32, 16, 18, 22, 56, 20]
+// let
+//     arr = [32, 16, 18, 22, 56, 20]
 
-function checkItems(val) {
-    return val % 2 === 0
+// function checkItems(val) {
+//     return val % 2 === 0
+// }
+// console.log(arr.every(checkItems))
+//     //task 1
+function isIsogram(str) {
+    str = str.toLowerCase();
+    for (let i = 0; i < str.length; ++i) {
+        for (let j = i + 1; j < str.length; ++j) {
+            if (str[i] === str[j]) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
-console.log(arr.every(checkItems))
-    //task 1
+console.log(isIsogram('vbarev'))
+
+
+//tsk 2
